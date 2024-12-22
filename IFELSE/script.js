6589718381c0dfea20b6ba28363b95c98f3120f9
nirajchand <149762449+nirajchand@users.noreply.github.com>
2024-12-22 09:38:25 +0545

    calculator task done

---
+++ IFELSE/script.js
@@ -0,0 +1,21 @@
+// let age = 12;
+// if(age < 18){
+//     console.log("YOu are not mature")
+// }else{
+//     console.log("You are mature")
+// }
+
+
+// let marks = prompt("please enter the marks")
+
+// if(marks < 20){
+//     console.log("Your future is dead")
+// }else if(marks < 30){
+//     console.log("you future is gede")
+// }else if(marks < 50){
+//     console.log("YOu are good")
+// }else{
+//     console.log("you have good future")
+// }
+
+
---
+++ Week2/node.js
@@ -0,0 +1,15 @@
+// const sym1 = Symbol("Hello")
+
+// const sym2 = Symbol("Hello")
+// console.log(sym1 === sym2)
+
+// function fun(){
+//     var x = 20;
+//     console.log(x);
+// }
+// fun()
+
+
+let details = {name: "Rob", age: 20}
+let nam = ["NIraj", "Hari","Nari"]
+console.log(typeof(nam))
